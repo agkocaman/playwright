d5d5b6a8f2e62b9f66454ccd89487e836cd83e8d
agkocaman <ggokhankocaman@gmail.com>
2023-01-04 11:19:55 +0300

    PNG

--- screenshot.js
+++ screenshot.js
@@ -5,14 +5,15 @@ const { chromium } = require('playwright');
     // function code
   
   	// launching browser
-    const browser = await chromium.launch({ headless:false , slowMo:100 });
+    const browser = await chromium.launch();
   	// creating a page inside browser
   	const page = await browser.newPage();
    	// navigating to site
-    await page.goto('https://google.com/');
+    await page.goto('https://www.haberturk.com/');
     await page.screenshot({path:'deneme.png'});
-    const header = await page.$('#gb');
+    const header = await page.$('#MarketHorizontal');
     await header.screenshot({path : 'header.png'})
+    await page.screenshot({path:'full.png', fullPage:true});
   	// closing browser
     await browser.close();
   
